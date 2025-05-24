3a2782d71efd6881b7a3db4a76b524da13f6f92d
Ryan Hamamura <58859899+ryanhamamura@users.noreply.github.com>
2025-05-23 16:52:26 -1000

    fix(revert): changed my mind about italics

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -8,10 +8,10 @@ local M = {}
 M.base46 = {
 	theme = "kanagawa",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 
   integrations = {
     'render-markdown',
