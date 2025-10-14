975df96109a7d7b04a5728428898b80ede0790d6
Ryan Hamamura <58859899+ryanhamamura@users.noreply.github.com>
2025-10-14 07:56:33 -1000

    chore: update config settings and nvim packages
    - Disabled always-thinking mode in Claude settings
    - Configured alacritty to use tmux as default shell
    - Added Python 3 to mise tools
    - Updated nvim lazy-lock and lazyvim packages

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Keep cursor near center unless at beginning or end of file
+vim.opt.scrolloff = 999
