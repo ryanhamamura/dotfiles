5896dadf12f9c76e22e8e913820c2efe0f51f08e
Ryan Hamamura <58859899+ryanhamamura@users.noreply.github.com>
2025-10-17 20:00:01 -1000

    fix: add true color support for neovim/tmux

---
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/everforest" },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
